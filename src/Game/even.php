23e2b78220c57064bd3ea4fd47e3b5a76ca2055d
aktm1982 <aktm@yandex.ru>
2020-11-23 23:08:48 +0700

    Execute phpcs notes

--- src/Game/even.php
+++ src/Game/even.php
@@ -2,25 +2,24 @@
 
 namespace Brain\Game\Even;
 
-use function \cli\line;
-use function \cli\prompt;
+use function cli\line;
+use function cli\prompt;
 
-function getCorrectAnswer(int $targetNumber) : string
+function getCorrectAnswer(int $targetNumber): string
 {
-	if($targetNumber % 2 === 0) {
-		return 'yes';
-	}
-	
-	return 'no';
+    if ($targetNumber % 2 === 0) {
+        return 'yes';
+    }
+    return 'no';
 }
 
-function getResult() : array
+function getResult(): array
 {
-	$result = [];
-	$targetNumber = mt_rand(1,100);
-	line("Question: $targetNumber");
-	$result['userInput'] = trim(prompt('Your answer'));
-	$result['correctAnswer'] = getCorrectAnswer($targetNumber);
-	$result['isCorrect'] = $result['userInput'] === $result['correctAnswer'];
-	return $result;
+    $result = [];
+    $targetNumber = mt_rand(1, 100);
+    line("Question: $targetNumber");
+    $result['userInput'] = trim(prompt('Your answer'));
+    $result['correctAnswer'] = getCorrectAnswer($targetNumber);
+    $result['isCorrect'] = $result['userInput'] === $result['correctAnswer'];
+    return $result;
 }
