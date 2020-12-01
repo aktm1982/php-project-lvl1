01b588c767e399995d23a4ac8d79ec7fe238fd63
aktm1982 <aktm@yandex.ru>
2020-12-01 23:33:40 +0700

    modify bin path

---
+++ src/Game/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Brain\Game\Settings;
+
+const INIT_SCORE   = 0;
+const TARGET_SCORE = 3;
+
+const MIN_VALUE    = 1;
+const MAX_VALUE    = 100;
+
+const OPERATORS    = ['+', '-'];
