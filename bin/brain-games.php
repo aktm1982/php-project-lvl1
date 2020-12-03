9284ee6ced25ec5b14157409317dff3fd552c8b0
aktm1982 <aktm@yandex.ru>
2020-12-04 00:42:12 +0700

    add autoload to brain-games and Cli

--- bin/brain-games.php
+++ bin/brain-games.php
@@ -2,4 +2,15 @@
 
 <?php
 
-require_once __DIR__ . '/../src/Cli';
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use function cli\line;
+
+line('Welcome to the Brain Games!');
