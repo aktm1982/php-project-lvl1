a8afb806f4434072ac4cec5ebc167957f9b9d865
aktm1982 <aktm@yandex.ru>
2020-12-21 16:40:51 +0700

    update brain-prime game logic

--- src/Game/prime/prime.php
+++ src/Game/prime/prime.php
@@ -20,6 +20,10 @@ function initGame()
     }
 
     $isPrime = function (int $number): bool {
+        if ($number === 1) {
+            return false;
+        }
+
         for ($i = 2; $i < $number / 2; $i++) {
             if ($number % $i === 0) {
                 return false;
